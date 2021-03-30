61d519d1e2c6d148e2568038255d98426a331733
ismobaga <ismobaga@users.noreply.github.com>
2021-03-30 17:50:31 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
